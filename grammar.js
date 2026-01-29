a2686a15ec23b81c3ed5c7ef492a15ce694bd9b1
StrongTheDev <adhueywilan@gmail.com>
2026-01-29 21:47:19 +0300

    Update grammar.js again

--- grammar.js
+++ grammar.js
@@ -82,7 +82,7 @@ module.exports = grammar({
     ),
 
     whitespace: $ => /\s+/,
-    comment: $ => seq('#', /.*\n?/),
+    comment: $ => seq('#', /.*?/),
     newline: $ => /\n/,
 
     string: $ => /"([^"\\]|\\.)*"/,
